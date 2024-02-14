4eda65b9c68f1294d8439c8c1f979da70777bd6d
samsul kobir <makkobir79@gmail.com>
2024-02-14 22:42:32 +0200

    first commit

---
+++ __notes.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * --- part 2  ---
+ * (implement heart)
+ * A data model for "likes" that relates each one to a specific message
+ * A mutation function that saves a new like to the database
+ * A button in the UI that triggers the function
+ * A query that counts how many times a message has been liked
+ *
+ */
